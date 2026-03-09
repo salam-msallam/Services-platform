b2621c817632fc4e06174c3b05af627db916d78e
salam-msallam <msallamsalma4@gmail.com>
2026-03-09 12:14:08 +0200

    Implement initial database schema and structure

---
+++ database/migrations/2025_03_08_000009_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\StatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('business_account_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('service_id')->constrained()->cascadeOnDelete();
+            $table->string('status')->default(StatusEnum::Pending->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
