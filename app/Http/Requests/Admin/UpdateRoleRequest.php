881f3660c475516ddff3ed43a8f372ed38ead8b5
salam-msallam <msallamsalma4@gmail.com>
2026-03-21 14:43:32 +0300

    build roles system

---
+++ app/Http/Requests/Admin/UpdateRoleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Spatie\Permission\Models\Role;
+
+class UpdateRoleRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return (bool) $this->user()?->can('manage roles');
+    }
+
+    /**
+     * @return array<string, array<int, \Illuminate\Contracts\Validation\ValidationRule|string>>
+     */
+    public function rules(): array
+    {
+        /** @var Role $role */
+        $role = $this->route('role');
+
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('roles', 'name')
+                    ->where('guard_name', 'web')
+                    ->ignore($role->id),
+            ],
+        ];
+    }
+}
